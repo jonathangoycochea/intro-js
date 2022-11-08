b532b438d5563fe3a40430993e1ff835aea18755
Jonathan Goycochea <jonathan.aguilargoycochea@gmail.com>
2022-11-08 19:14:40 -0300

    Intro-uno-js: First steps in JS

---
+++ index.js
@@ -0,0 +1,14 @@
+let cityName = "Junin"
+let randomNumber = 313
+
+cityName = randomNumber
+
+console.log(`cityName variable value: ${cityName}`)
+console.log(`randomNumber variable value: ${randomNumber}`)
+
+const NAME = "Jonathan Gabriel"
+const SURNAME = "Aguilar Goycochea"
+
+const FULLNAME = `${NAME}, ${SURNAME}`
+
+console.log(`Fullname: ${FULLNAME}`)
